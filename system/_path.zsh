f4e8e10d8cf09db1930826a2963817f99e546f6c
undermoodzyx <mygmzyx@gmail.com>
2022-12-03 10:12:23 +0800

    ✨ feat(dotfiles): customize

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,3 @@
 export PATH="./bin:$HOME/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+# export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
